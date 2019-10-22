67a41e1cc55aef1bd7a37c787b404174d953c626
Dat <dat.huynh.5395@gmail.com>
2019-10-21 23:49:59 -0600

    done prob 1

--- no1/subnet.py
+++ no1/subnet.py
@@ -13,15 +13,25 @@ for opt, val in optlist:
     elif opt == "-o":
         output = open(val, "w")
 
+try:
+    input
+except NameError:
+    input = open("input", "r")
+
+try:
+    output
+except NameError:
+    output = open("output", "w")
+
 for line in input:
     ip_hex = line.split()[0]
-    ip = [int(ip_hex[octet:octet+2],16) for octet in range(2, len(ip_hex), 2)]
-    octets = [ip_hex[octet:octet+2] for octet in range(2, len(ip_hex), 2)]
-    ip = [int(i,16) for i in octets]
-
+    ip_str = ".".join([str(int(ip_hex[octet:octet+2],16)) for octet in range(2, len(ip_hex), 2)])
     cidr = line.split()[1]
-    sn_ip = cidr.split('/')[0].split('.')
+    sn_ip = cidr.split('/')[0]
     mask = cidr.split('/')[1]
-    print(sn_ip)
-    print(mask)
 
+    ip_num = int(ip_hex[2:], 16)
+    sn_ip_num = struct.unpack("!I",socket.inet_aton(sn_ip))[0]
+    mask_num = (0xffffffff >> (32 - int(mask))) << (32 - int(mask))
+    print(ip_str, '\t', cidr, '\t', (sn_ip_num&mask_num == ip_num&mask_num))
+    output.write(ip_str + '\t' + cidr + '\t' + str(sn_ip_num&mask_num == ip_num&mask_num) + '\n')
